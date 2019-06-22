fe40467ba3e57c26b0edfc203b31a1a349832d3b
Deepika <34454932+16deepikaramesh@users.noreply.github.com>
2019-06-21 21:38:46 -0700

    Add files via upload

---
+++ Black_line_follower.ino
@@ -0,0 +1,49 @@
+int l,r;                                   // initializing two variables l & r for storing inputs from Light sensors.
+void setup()
+{
+  pinMode(10,OUTPUT);                     //10,11 for left motor
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);                     //12,13 for right motor
+  pinMode(13,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(4,INPUT);                       // At pin no:4 we will connect the right light sensor which will act as an input device
+  pinMode(3,INPUT);                       // At pin no:3 we will connect the left light sensor which will act as input device.
+  digitalWrite(2,HIGH);
+  digitalWrite(5,LOW);
+}
+void loop()
+{
+  r=digitalRead(4);                       // Reading the input of right sensor and storing the value in pin no:4 of Arduino
+  l=digitalRead(3);                       // Reading the input of left sensor and storing the value in pin no:3 of Arduino
+
+  if(r==HIGH && l==HIGH)                  // If both left & right sensor are on white surface
+  {                                       // the left & right motor will go forward
+    digitalWrite(10,HIGH);
+    digitalWrite(11,LOW);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,LOW);
+  }
+  else if(l==HIGH && r==LOW)             // If left sensor is on white and right sensor is on black surface
+  {                                      // the left motor will go forward & right motor will go backward
+    digitalWrite(10,HIGH);               
+    digitalWrite(11,LOW);
+    digitalWrite(12,LOW);
+    digitalWrite(13,LOW);
+  }
+  else if(l==LOW && r==HIGH)            // If left sensor is on white and right sensor is on black surface
+  {                                     // the left motor will go backward & right motor will go forward.
+    digitalWrite(10,LOW);
+    digitalWrite(11,LOW);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,LOW);
+   }
+    else                           // the last condition is, if both left & right light sensor are on black surface
+  {                                // the left & right will stop. 
+    digitalWrite(10,HIGH);
+    digitalWrite(11,HIGH);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,HIGH);
+   }
+}
+    
